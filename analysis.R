03cde6129440bee4e78dd9bf88cc5255ab188739
WHTCrawford <william.h.crawford@icloud.com>
2018-02-19 12:09:13 +0000

    pi

--- analysis.R
+++ analysis.R
@@ -1,3 +1,5 @@
+# Import -----
+
 rm(list = ls(all=T))
 suppressMessages(suppressWarnings(library(reshape2)))
 library(plyr)
@@ -9,10 +11,13 @@ library(lattice)
 source('/Users/Billy/PycharmProjects/GALR/mulitplot.R')
 library(entropy)
 library(FNN)
+library(pbapply)
 
 
+# Objective ----
+# Objective 1 is min log(1-D(G(z)), Objective 2 is max log(D(G(z))
 
-### Colours ------------
+# Colours ------------
 
 
 gg_color_hue <- function(n) {
@@ -22,7 +27,24 @@ gg_color_hue <- function(n) {
 
 cols = gg_color_hue(2)
 
-### Data -------------------
+# Max density -------
+bin_number = 30
+pull_max_density =function(vec){
+  min_break = round_any(min(vec), diff(range(vec))/bin_number, floor)
+  max_break = round_any(max(vec), diff(range(vec))/bin_number, ceiling)
+  breaks = seq(min_break, max_break, diff(range(vec/bin_number)))
+  histo = hist(vec, breaks=breaks, plot=F)
+  return(max(histo$density))
+}
+
+
+# Actual density ----
+
+mixture_density = function(x){
+  return(dnorm(x,10,1)/2 + dnorm(x,0,1)/2)
+}
+
+# Data -------------------
 dataframes = list()
 
 item = 1
@@ -35,35 +57,9 @@ for(objective in 1:2){
     df = na.omit(df)
     dataframes[[item]] = df
     item =item+1
+    rm(df)
   }
 }
-
-
-# Find KL divergence ----
-length_vec = ncol(dataframes[[1]])-2
-which_mean = sample(c(0,10),size = length_vec, replace =T)
-real_samples = rnorm(n = length_vec,mean = which_mean,sd =1 )
-
-mode_collapse = rnorm(n = length_vec,mean = 10,sd =1 )
-
-# Kernel density plug in -------
-real_density = function(x){
-  return(0.5*dnorm(x,10,1)+0.5*dnorm(x,0,1))
-}
-
-KL_kernel_estimate = function(vec){
-  x_values = density(vec)$x
-  y_values = density(vec)$y
-  return(sum(real_density(x_values)*log(real_density(x_values)/y_values)))
-}
-
-KL_kernel_estimate(real_samples)
-KL_kernel_estimate(mode_collapse)
-
-# From paper (Do i need to do this, can just use k-nearest neighbours)-------
-points_in_each_segment = 20
-
-
 
 # K nearest neighbours ---------
 
@@ -74,14 +70,15 @@ real_dist = rnorm(n = length_vec,mean = which_mean,sd =1 )
 
 kl_to_real = function(vec){
   vec_1 = as.numeric(vec)
-  return(KL.divergence(real_dist,vec_1, k=10)[10])
+  return(KL.divergence(real_dist,vec_1, k=5)[5])
 }
 
 kl_by_row = function(df){
   return(as.numeric(apply(df[,3:ncol(df)],1, kl_to_real)))
 }
 
-kl_rows = lapply(X = dataframes,FUN = kl_by_row)
+
+kl_rows = pblapply(X = dataframes,FUN = kl_by_row)
 
 which_min_kl_div = lapply(kl_rows,FUN = function(vec){return(which.min(vec))})
 
@@ -90,126 +87,107 @@ for(item in 1:4){
   collected_rows_for_hist[[item]] = as.numeric(dataframes[[item]][which_min_kl_div[[item]],3:ncol(dataframes[[item]])])
 }
 
-bin_number = 50
-
-mixture_density = function(x){
-  return(dnorm(x,10,1)/2 + dnorm(x,0,1)/2)
+which.median = function(x) {
+  if (length(x) %% 2 != 0) {
+    which(x == median(x))
+  } else if (length(x) %% 2 == 0) {
+    a = sort(x)[c(length(x)/2, length(x)/2+1)]
+    which(x == a[1])
+  }
 }
 
-pull_max_density =function(vec){
-  min_break = round_any(min(vec), diff(range(vec))/bin_number, floor)
-  max_break = round_any(max(vec), diff(range(vec))/bin_number, ceiling)
-  breaks = seq(min_break, max_break, diff(range(vec/bin_number)))
-  histo = hist(vec, breaks=breaks, plot=F)
-  return(max(histo$density))
-}
-
-max_density = max(as.numeric(lapply(collected_rows_for_hist,pull_max_density)))
-
-p1 = ggplot(data.frame(x=collected_rows_for_hist[[1]]), aes(x)) + 
-  geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
-  stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
-  xlim(-5,15)+ylim(0,max_density)+labs(title='1 Minibatch')+ylab('Density')
-
-p2 = ggplot(data.frame(x=collected_rows_for_hist[[2]]), aes(x)) + 
-  geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
-  stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
-  xlim(-5,15)+ylim(0,max_density)+labs(title='1 No Minibatch')+ylab('Density')
+which_med_kl_div = lapply(kl_rows,which.median)
 
 
-p3 = ggplot(data.frame(x=collected_rows_for_hist[[3]]), aes(x)) + 
-  geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
-  stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
-  xlim(-5,15)+ylim(0,max_density)+labs(title='2 Minibatch')+ylab('Density')
+hist(as.numeric(dataframes[[4]][which_med_kl_div[[4]],3:ncol(dataframes[[4]])]))
 
-p4 = ggplot(data.frame(x=collected_rows_for_hist[[4]]), aes(x)) + 
-  geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
-  stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
-  xlim(-5,15)+ylim(0,max_density)+labs(title='2 No Minibatch')+ylab('Density')
+# Compare two median side by side general function ----
 
-multiplot(p1, p2,p3,p4, cols=2)
-
-
-# Find mean density values for each entry for each distribution in the mixture -------
-
-
-return_big_over_small = function(df){
-  low_gauss_densities = cbind(df[,1:2],apply(df[,3:ncol(df)], c(1,2),dnorm, mean = 0,sd = 1))
-  high_gauss_densities = cbind(df[,1:2],apply(df[,3:ncol(df)], c(1,2),dnorm, mean = 10,sd = 1))
-  
-  colnames(df)= colnames(low_gauss_densities)= colnames(high_gauss_densities)= c('learning_rate','Time',3:ncol(df))
-  
-  low_gauss_densities_means = rowMeans(low_gauss_densities[,3:ncol(df)])
-  high_gauss_densities_means = rowMeans(high_gauss_densities[,3:ncol(df)])
-  
-  means_df = data.frame(low = low_gauss_densities_means,high = high_gauss_densities_means)
-  
-  find_big_over_small = function(vec){
-    return(max(vec)/min(vec))
+plot_median_side_by_side = function(index1,index2,title1,title2, y_max=0.5){
+  collected_rows_for_hist_med = list()
+  indice = c(index1,index2)
+  for(item in indice){
+    collected_rows_for_hist_med[[item]] = as.numeric(dataframes[[item]][which_med_kl_div[[item]],3:ncol(dataframes[[item]])])
   }
   
-  big_over_small = as.numeric(apply(means_df,MARGIN = 1,find_big_over_small))
- 
-  return(big_over_small)
+  p1 = ggplot(data.frame(x=collected_rows_for_hist_med[[index1]]), aes(x)) + 
+    geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
+    stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
+    xlim(-5,15) + ylim(0,y_max)+
+    labs(title=title1)+ylab('Density')
+  
+  p2 = ggplot(data.frame(x=collected_rows_for_hist_med[[index2]]), aes(x)) + 
+    geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
+    stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
+    xlim(-5,15) + ylim(0,y_max)+
+    labs(title=title2)+ylab('Density')
+  
+  suppressWarnings(multiplot(p1, p2, cols=2))
+  
 }
 
+plot_median_side_by_side(1,2,'Minibatch Discrimination','No Minibatch Discrimination') # oringial objective
+plot_median_side_by_side(3,4,'Minibatch Discrimination','No Minibatch Discrimination') # alternative
+plot_median_side_by_side(2,4,'min log(1-D(G(z))','max log(D(G(z))') # non minibatch
 
-big_over_small_list = lapply(dataframes,return_big_over_small)
+# Compare two best side by side general function ----
 
-which_closest_to_1 = function(vec){
-  vec_minus_1 = abs(vec-1)
-  best = which.min(vec_minus_1)
-  return(best)
+plot_best_side_by_side = function(index1,index2,title1,title2, y_max=0.5){
+  collected_rows_for_hist = list()
+  indice = c(index1,index2)
+  for(item in indice){
+    collected_rows_for_hist[[item]] = as.numeric(dataframes[[item]][which_min_kl_div[[item]],3:ncol(dataframes[[item]])])
+  }
+  
+  p1 = ggplot(data.frame(x=collected_rows_for_hist[[index1]]), aes(x)) + 
+    geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
+    stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
+    xlim(-5,15) + ylim(0,y_max)+
+    labs(title=title1)+ylab('Density')
+  
+  p2 = ggplot(data.frame(x=collected_rows_for_hist[[index2]]), aes(x)) + 
+    geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
+    stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
+    xlim(-5,15) + ylim(0,y_max)+
+    labs(title=title2)+ylab('Density')
+  
+  suppressWarnings(multiplot(p1, p2, cols=2))
+  
 }
 
-best_data_rows = lapply(big_over_small_list,which_closest_to_1)
+plot_best_side_by_side(1,2,'Minibatch Discrimination','No Minibatch Discrimination') # oringial objective
+plot_best_side_by_side(3,4,'Minibatch Discrimination','No Minibatch Discrimination') # alternative
+plot_best_side_by_side(2,4,'min log(1-D(G(z))','max log(D(G(z))') # non minibatch
 
-collected_rows_for_hist = list()
-for(item in 1:4){
-  collected_rows_for_hist[[item]] = as.numeric(dataframes[[item]][best_data_rows[[item]],3:ncol(dataframes[[item]])])
+
+# Pick out 25% quantile and plot ------
+which.25 = function(x) {
+  return(which(x == sort(x)[round(length(x)/4)]))
 }
 
+which_25_kl_div = lapply(kl_rows,which.25)
 
-bin_number = 50
 
-mixture_density = function(x){
-  return(dnorm(x,10,1)/2 + dnorm(x,0,1)/2)
+plot_25 = function(data_index,title1 = '',y_max=0.5){
+  bin_number = 40
+  data = as.numeric(dataframes[[data_index]][which_25_kl_div[[data_index]],3:ncol(dataframes[[data_index]])])
+  ggplot(data.frame(x=data), aes(x)) + 
+    geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
+    stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
+    xlim(-5,15) + ylim(0,y_max)+
+    labs(title=title1)+ylab('Density')
 }
 
-pull_max_density =function(vec){
-  min_break = round_any(min(vec), diff(range(vec))/bin_number, floor)
-  max_break = round_any(max(vec), diff(range(vec))/bin_number, ceiling)
-  breaks = seq(min_break, max_break, diff(range(vec/bin_number)))
-  histo = hist(vec, breaks=breaks, plot=F)
-  return(max(histo$density))
-}
+plot_25(2) # oringinal, no minibatch
 
-max_density = max(as.numeric(lapply(collected_rows_for_hist,pull_max_density)))
-
-p1 = ggplot(data.frame(x=collected_rows_for_hist[[1]]), aes(x)) + 
-  geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
-  stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
-  xlim(-5,15)+ylim(0,max_density)+labs(title='1 Minibatch')+ylab('Density')
-
-p2 = ggplot(data.frame(x=collected_rows_for_hist[[2]]), aes(x)) + 
-  geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
-  stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
-  xlim(-5,15)+ylim(0,max_density)+labs(title='1 No Minibatch')+ylab('Density')
-
-
-p3 = ggplot(data.frame(x=collected_rows_for_hist[[3]]), aes(x)) + 
-  geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
-  stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
-  xlim(-5,15)+ylim(0,max_density)+labs(title='2 Minibatch')+ylab('Density')
-
-p4 = ggplot(data.frame(x=collected_rows_for_hist[[4]]), aes(x)) + 
-  geom_histogram(fill = cols[2],aes(y = ..density..),bins = bin_number)+
-  stat_function(fun = mixture_density, lwd = 1, col = cols[1])+
-  xlim(-5,15)+ylim(0,max_density)+labs(title='2 No Minibatch')+ylab('Density')
-
-multiplot(p1, p2,p3,p4, cols=2)
+plot_25(4) # new, no minibatch
 
 
 
 
+# Find average KL by group -----
+means = as.numeric(lapply(kl_rows, mean))
+means = data.frame(original_objective = means[1:2], changed_objective = means[3:4]) 
+rownames(means) = c('Minibatch','No minibatch') 
+means
 
